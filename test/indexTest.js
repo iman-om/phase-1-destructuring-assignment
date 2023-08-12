f2bce990d2c8ceb6c863559e81bce1109d2f6c08
imane omari <imane3.omari@gmail.com>
2023-08-12 04:05:26 +0100

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 require ( './helpers.js' );
 
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 describe("index.js", () => {
   it("String: uses destructuring to declare five animal sounds", () => {
